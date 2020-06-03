4cf3cf898ecedf35ba368adf11ca22b363dbfb40
Johnothan King <johnothanking@protonmail.com>
2020-06-03 01:15:36 -0700

    Fix a typo in the error message

--- timelim.c
+++ timelim.c
@@ -294,7 +294,7 @@ int main(int argc, char *argv[])
                 multiplier = year * 1000;
                 goto nano;
             default: // Reject invalid arguments
-                printf("`%c` not a valid suffix!\n", number[suffix_location]);
+                printf("`%c` is not a valid suffix!\n", number[suffix_location]);
                 return 1;
         }
 
